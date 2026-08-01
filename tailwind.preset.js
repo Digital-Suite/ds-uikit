9e24f6f5ede7f42cdd5b1489a795d84032236264
Code Head <iamjaime89@gmail.com>
2026-08-01 19:34:48 -0400

    feat: add global theme.css and tailwind preset

---
+++ tailwind.preset.js
@@ -0,0 +1,28 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        'primary-dark': 'var(--color-primary-dark)',
+        'primary-light': 'var(--color-primary-light)',
+        'primary-subtle': 'var(--color-primary-subtle)',
+        'primary-glow': 'var(--color-primary-glow)',
+        
+        accent: 'var(--color-accent)',
+        'accent-subtle': 'var(--color-accent-subtle)',
+
+        bg: 'var(--color-bg)',
+        surface: 'var(--color-surface)',
+        'surface-raised': 'var(--color-surface-raised)',
+        black: 'var(--color-black)',
+
+        border: 'var(--color-border)',
+        'border-subtle': 'var(--color-border-subtle)',
+
+        text: 'var(--color-text)',
+        'text-muted': 'var(--color-text-muted)',
+        'text-faint': 'var(--color-text-faint)',
+      },
+    }
+  }
+};
